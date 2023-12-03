20799b60878068da2435b3aee48dd167144cfdb8
kyawzinhtett <itskyawzinhtet@gmail.com>
2023-12-03 23:36:20 +0630

    init: project setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
