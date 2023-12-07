a291a0e074a0782ed3f450d4f63d093673f8c259
kyawzinhtett <itskyawzinhtet@gmail.com>
2023-12-07 14:49:44 +0630

    feat: Header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <nav>
-      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-      <RouterLink :to="{ name: 'stake' }">Stake</RouterLink>
-    </nav>
-  </header>
+  <!-- Header -->
+  <Header />
+  <!-- SideBar -->
+  <nav>
+    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+    <RouterLink :to="{ name: 'stake' }">Stake</RouterLink>
+  </nav>
 
   <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/components/Header.vue';
+</script>
 
 <style scoped></style>
