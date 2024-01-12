24b9662d718d3d5cadbacaed54620a1db7b084e9
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-01-12 23:25:58 +0630

    feat: responsive wallet view

--- src/views/Stake/StakeView.vue
+++ src/views/Stake/StakeView.vue
@@ -1,54 +1,52 @@
 <template>
-    <template class="flex justify-center">
-        <div class="mt-10">
-            <h1 class="font-semibold mb-3">All Pools</h1>
-            <div class="card md:flex items-center gap-10 mb-14">
-                <img class="w-[240px] mb-7" src="/image-1.png" alt="Ethereum Eth">
-                <div class="w-full">
-                    <h1 class="font-bold md:text-xl mb-6">ETH/USDT</h1>
-                    <div class="flex justify-between mb-3">
-                        <div>
-                            <p class="text-xs text-gray mb-3">Total Liquidity</p>
-                            <p class="md:text-lg"> 1,329,732.61 USD</p>
-                        </div>
-                        <div>
-                            <p class="text-xs text-gray mb-3">APY</p>
-                            <p class="md:text-lg text-indigo">4.5% - 4.8%</p>
-                        </div>
+    <div class="px-5 md:px-10 lg:px-20 mt-10">
+        <h1 class="font-semibold mb-3">All Pools</h1>
+        <div class="card md:flex items-center gap-10 mb-14">
+            <img class="w-[240px] mb-7" src="/image-1.png" alt="Ethereum Eth">
+            <div class="w-full">
+                <h1 class="font-bold md:text-xl mb-6">ETH/USDT</h1>
+                <div class="flex justify-between mb-3">
+                    <div>
+                        <p class="text-xs text-gray mb-3">Total Liquidity</p>
+                        <p class="md:text-lg"> 1,329,732.61 USD</p>
                     </div>
-                    <div class="bg-[#141414] rounded-3xl text-center px-2 xl:px-28 py-3 mb-6">
-                        <span class="text-xs md:text-sm">Up to 4.8% of return by $5,000 invested</span>
+                    <div>
+                        <p class="text-xs text-gray mb-3">APY</p>
+                        <p class="md:text-lg text-indigo">4.5% - 4.8%</p>
                     </div>
-                    <router-link :to="{ name: 'pool' }">
-                        <button class="btn-primary text-sm md:text-base">Stake</button>
-                    </router-link>
                 </div>
-            </div>
-
-            <div class="card md:flex items-center gap-10 mb-14">
-                <img class="w-[240px] mb-7" src="/image-2.png" alt="Tron Trx">
-                <div class="w-full">
-                    <h1 class="font-bold md:text-xl mb-6">TRX/USDT</h1>
-                    <div class="flex justify-between mb-3">
-                        <div>
-                            <p class="text-xs text-gray mb-3">Total Liquidity</p>
-                            <p class="md:text-lg"> 1,001,761.32 USD</p>
-                        </div>
-                        <div>
-                            <p class="text-xs text-gray mb-3">APY</p>
-                            <p class="md:text-lg text-indigo">3.5% - 4.0%</p>
-                        </div>
-                    </div>
-                    <div class="bg-[#141414] rounded-3xl text-center px-2 xl:px-28 py-3 mb-6">
-                        <span class="text-xs md:text-sm">Up to 4.0% of return by $4,000 invested</span>
-                    </div>
-                    <router-link :to="{ name: 'pool' }">
-                        <button class="btn-primary text-sm md:text-base">Stake</button>
-                    </router-link>
+                <div class="bg-[#141414] rounded-3xl text-center px-2 xl:px-28 py-3 mb-6">
+                    <span class="text-xs md:text-sm">Up to 4.8% of return by $5,000 invested</span>
                 </div>
+                <router-link :to="{ name: 'pool' }">
+                    <button class="btn-primary text-sm md:text-base">Stake</button>
+                </router-link>
             </div>
         </div>
-    </template>
+
+        <div class="card md:flex items-center gap-10 mb-14">
+            <img class="w-[240px] mb-7" src="/image-2.png" alt="Tron Trx">
+            <div class="w-full">
+                <h1 class="font-bold md:text-xl mb-6">TRX/USDT</h1>
+                <div class="flex justify-between mb-3">
+                    <div>
+                        <p class="text-xs text-gray mb-3">Total Liquidity</p>
+                        <p class="md:text-lg"> 1,001,761.32 USD</p>
+                    </div>
+                    <div>
+                        <p class="text-xs text-gray mb-3">APY</p>
+                        <p class="md:text-lg text-indigo">3.5% - 4.0%</p>
+                    </div>
+                </div>
+                <div class="bg-[#141414] rounded-3xl text-center px-2 xl:px-28 py-3 mb-6">
+                    <span class="text-xs md:text-sm">Up to 4.0% of return by $4,000 invested</span>
+                </div>
+                <router-link :to="{ name: 'pool' }">
+                    <button class="btn-primary text-sm md:text-base">Stake</button>
+                </router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
--- src/views/Stake/WalletView.vue
+++ src/views/Stake/WalletView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="h-screen">
-        <section class="mt-8">
+    <div class="h-full py-2 text-sm md:text-base">
+        <section class="mt-8 mb-8">
             <router-link :to="{ name: 'home' }">
                 <span class="text-gray">Home&nbsp;</span>
             </router-link>
@@ -12,61 +12,61 @@
             </router-link>
         </section>
 
-        <h1 class="text-center font-bold text-3xl">Connect Wallet</h1>
+        <h1 class="text-center font-bold text-xl md:text-3xl">Connect Wallet</h1>
 
-        <section class="grid grid-cols-12 mt-12 gap-12">
-            <div v-if="isTrustWalletSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectBscWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Binance</span>
-                    <img src="/binance.svg" alt="Binance" class="w-[50px]">
+        <section class="md:flex mt-12 md:mb-5 gap-5 md:gap-10">
+            <div v-if="isTrustWalletSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectBscWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Binance</span>
+                    <img src="/binance.svg" alt="Binance" class="w-[40px] h-[40px]">
                 </div>
             </div>
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Coinbase</span>
-                    <img src="/coinbase.svg" alt="Coinbase" class="w-[50px]">
-                </div>
-            </div>
-        </section>
-        <section class="grid grid-cols-12 mt-12 gap-12">
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Kraken</span>
-                    <img src="/kraken.svg" alt="Kraken" class="w-[50px]">
-                </div>
-            </div>
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Bybit</span>
-                    <img src="/bybit.svg" alt="Bybit" class="w-[50px]">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Coinbase</span>
+                    <img src="/coinbase.svg" alt="Coinbase" class="w-[40px] h-[40px]">
                 </div>
             </div>
         </section>
-        <section class="grid grid-cols-12 mt-12 gap-12">
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">OKX</span>
-                    <img src="/okx.svg" alt="OKX" class="w-[50px]">
+        <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Kraken</span>
+                    <img src="/kraken.svg" alt="Kraken" class="w-[40px] h-[40px]">
                 </div>
             </div>
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Crypto.com</span>
-                    <img src="/crypto.svg" alt="Crypto" class="w-[50px]">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Bybit</span>
+                    <img src="/bybit.svg" alt="Bybit" class="w-[40px] h-[40px]">
                 </div>
             </div>
         </section>
-        <section class="grid grid-cols-12 mt-12 gap-12">
-            <div v-if="isMetamaskSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Metamask</span>
-                    <img src="/metamask.svg" alt="Metamask" class="w-[50px]">
+        <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">OKX</span>
+                    <img src="/okx.svg" alt="OKX" class="w-[40px] h-[40px]">
                 </div>
             </div>
-            <div v-if="isTrustWalletSupported" class="col-span-12 lg:col-span-6 hover:cursor-pointer">
-                <div @click="connectBscWallet" class="bg-tertiary rounded-md flex justify-between items-center px-10 py-5">
-                    <span class="text-2xl">Trustwallet</span>
-                    <img src="/trust-wallet.svg" alt="Trustwallet" class="w-[50px]">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Crypto.com</span>
+                    <img src="/crypto.svg" alt="Crypto" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+        </section>
+        <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div v-if="isMetamaskSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Metamask</span>
+                    <img src="/metamask.svg" alt="Metamask" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+            <div v-if="isTrustWalletSupported" class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectBscWallet" class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Trustwallet</span>
+                    <img src="/trust-wallet.svg" alt="Trustwallet" class="w-[40px] h-[40px]">
                 </div>
             </div>
         </section>
