1abfd7a7cf45cac9762ad32feff5deb9e1944506
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-01-13 15:31:35 +0630

    feat: responsive about view

--- src/views/About/AboutView.vue
+++ src/views/About/AboutView.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="px-8 py-10">
+    <div class="md:px-8 py-10">
         <h1 class="bg-tertiary text-xl font-semibold rounded px-5 py-4 mb-3">Who We Are?</h1>
 
         <div class="p-5 mb-4">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt exercitationem deleniti
+            <p class="text-sm md:text-base">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus incidunt exercitationem deleniti
                 necessitatibus obcaecati dolores aspernatur soluta nemo! Culpa ratione quos praesentium labore. Nulla cumque
                 fuga numquam neque quasi quos ratione amet, placeat provident voluptatibus perferendis commodi voluptas
                 dignissimos deleniti consectetur eligendi ipsa corporis quibusdam eius nemo ullam facere qui accusantium?
@@ -13,42 +13,42 @@
 
             <br>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolor at dolores illo exercitationem culpa hic
+            <p class="text-sm md:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolor at dolores illo exercitationem culpa hic
                 ducimus dolorem repellendus tenetur. Dolorem eum omnis repellat asperiores voluptate mollitia eius ipsa
                 vitae.</p>
         </div>
 
-        <h3 class="p-5 text-lg font-semibold">Our Business Partners</h3>
+        <h3 class="p-5 text-lg font-semibold mb-5">Our Business Partners</h3>
 
-        <div class="flex justify-between p-5">
-            <div class="flex justify-between items-center gap-5">
+        <div class="md:flex justify-between px-5 md:mb-5 text-sm md:text-base">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>Coinbase</span>
                 <img src="/coinbase.svg" alt="Coinbase" class="w-[40px]">
             </div>
 
-            <div class="flex justify-between items-center gap-5">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>Metamask</span>
                 <img src="/metamask.svg" alt="Metamask" class="w-[40px]">
             </div>
 
-            <div class="flex justify-between items-center gap-5">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>OKX</span>
                 <img src="/okx.svg" alt="OKX" class="w-[40px]">
             </div>
         </div>
 
-        <div class="flex justify-between p-5">
-            <div class="flex justify-between items-center gap-5">
+        <div class="md:flex justify-between px-5 text-sm md:text-base">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>Phantom</span>
                 <img src="/phantom.svg" alt="Phantom" class="w-[40px]">
             </div>
 
-            <div class="flex justify-between items-center gap-5">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>Wallet Connect</span>
                 <img src="/wallet-connect.svg" alt="Wallet Connect" class="w-[40px]">
             </div>
 
-            <div class="flex justify-between items-center gap-5">
+            <div class="flex justify-between items-center gap-5 mb-3">
                 <span>Huobi</span>
                 <img src="/huobi.svg" alt="Huobi" class="w-[40px]">
             </div>
