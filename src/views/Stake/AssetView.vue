e061d7207860d2b50a788ae7a5e3a93bac82a723
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-01-21 15:13:55 +0630

    feat: update asset view

--- src/views/Stake/AssetView.vue
+++ src/views/Stake/AssetView.vue
@@ -1,38 +1,9 @@
 <template>
-    <div class="md:px-20 pb-10">
-        <Card class="mt-4 bg-grey rounded-md text-sm md:text-base">
-            <template #subtitle>
-                <h1 class="text-white text-lg font-bold md:mx-12 pt-3">Overall</h1>
-            </template>
-            <template #content>
-                <div class="md:flex justify-between items-center md:mx-12">
-                    <div class="md:leading-8">
-                        <span class="text-xs">Principal</span>
-                        <p class="text-white">2,567.84 USD</p>
-                    </div>
-                    <div class="md:leading-8">
-                        <span class="text-xs">Liquidity</span>
-                        <p class="text-white">4,785.31 USD</p>
-                    </div>
-                    <div class="md:leading-8">
-                        <span class="text-xs">Earn</span>
-                        <p class="text-white">58.4190 USDT</p>
-                    </div>
-                    <div class="md:leading-8 mb-5">
-                        <span class="text-xs">Claimable</span>
-                        <p class="text-white">4.8931 USDT</p>
-                    </div>
-                    <router-link :to="{ name: 'claim' }">
-                        <button class="btn-primary px-5 text-sm">Claim All</button>
-                    </router-link>
-                </div>
-            </template>
-        </Card>
-
-        <Card class="mt-8 bg-secondary rounded-md border border-gray-600 text-sm md:text-base">
+    <div class="md:px-20 h-screen">
+        <!-- <Card class="mt-4 bg-secondary rounded-md border border-gray-600 text-sm md:text-base">
             <template #subtitle>
                 <div class="flex justify-between items-center md:mx-12">
-                    <h1 class="text-white text-lg font-bold">ETH/USDT</h1>
+                    <h1 class="text-white text-lg font-bold">USDT</h1>
                     <p class="text-xs">APY <span class="text-indigo">8%</span></p>
                 </div>
             </template>
@@ -70,46 +41,67 @@
                     </div>
                 </section>
             </template>
-        </Card>
+        </Card> -->
 
-        <Card class="mt-8 bg-secondary rounded-md border border-gray-600 text-sm md:text-base">
+        <Card class="mt-4 bg-secondary rounded-md border border-gray-600 text-sm md:text-base">
             <template #subtitle>
                 <div class="flex justify-between items-center md:mx-12">
-                    <h1 class="text-white text-lg font-bold">TRX/USDT</h1>
-                    <p class="text-xs">APY <span class="text-indigo">8%</span></p>
+                    <div class="flex items-center gap-3">
+                        <img src="/usdt.svg" alt="USDT" class="w-[24px]">
+                        <h1 class="text-white text-lg font-bold">USDT</h1>
+                    </div>
+                    <router-link :to="{ name: 'claim' }">
+                        <button class="btn-primary text-sm px-4 py-2 md:px-10 md:py-3">Claim</button>
+                    </router-link>
                 </div>
             </template>
             <template #content>
                 <section class="md:mx-12">
                     <div class="md:flex justify-between items-center mb-6">
                         <div class="md:leading-8">
-                            <span class="text-xs">Principal</span>
+                            <span class="text-xs">Statistics</span>
                             <p class="text-white">2,567.84 USD</p>
                         </div>
                         <div class="md:leading-8">
-                            <span class="text-xs">Liquidity</span>
+                            <span class="text-xs">Frozen</span>
                             <p class="text-white">4,785.31 USD</p>
                         </div>
                         <div class="md:leading-8">
-                            <span class="text-xs">Liquidity Price</span>
+                            <span class="text-xs">Available</span>
                             <p class="text-white">4,785.31 USD</p>
-                        </div>
-                        <div class="md:leading-8">
-                            <span class="text-xs">Pool Liquidity</span>
-                            <p class="text-white">4.8931 USDT</p>
-                        </div>
-                        <div class="md:leading-8">
-                            <span class="text-xs">Earn</span>
-                            <p class="text-white">58.4190 USDT</p>
                         </div>
                     </div>
-                    <p class="mb-2 text-sm">Claimable</p>
-                    <div class="md:flex gap-4 items-center">
-                        <p class="text-indigo mr-6 mb-3">3.793 USDT</p>
-                        <router-link :to="{ name: 'claim' }">
-                            <button class="btn-primary text-sm mr-2">Claim</button>
-                        </router-link>
-                        <button class="btn-outline text-sm">Add</button>
+                </section>
+            </template>
+        </Card>
+
+        <Card class="mt-4 bg-secondary rounded-md border border-gray-600 text-sm md:text-base">
+            <template #subtitle>
+                <div class="flex justify-between items-center md:mx-12">
+                    <div class="flex items-center gap-3">
+                        <img src="/ether.svg" alt="Ether" class="w-[24px]">
+                        <h1 class="text-white text-lg font-bold">ETH</h1>
+                    </div>
+                    <router-link :to="{ name: 'claim' }">
+                        <button class="btn-primary text-sm px-4 py-2 md:px-10 md:py-3">Swap</button>
+                    </router-link>
+                </div>
+            </template>
+            <template #content>
+                <section class="md:mx-12">
+                    <div class="md:flex justify-between items-center mb-6">
+                        <div class="md:leading-8">
+                            <span class="text-xs">Statistics</span>
+                            <p class="text-white">2,567.84 USD</p>
+                        </div>
+                        <div class="md:leading-8">
+                            <span class="text-xs">Frozen</span>
+                            <p class="text-white">4,785.31 USD</p>
+                        </div>
+                        <div class="md:leading-8">
+                            <span class="text-xs">Available</span>
+                            <p class="text-white">4,785.31 USD</p>
+                        </div>
                     </div>
                 </section>
             </template>
