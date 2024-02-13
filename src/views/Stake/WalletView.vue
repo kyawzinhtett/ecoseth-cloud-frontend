5f02349405a9baeb8f11ab79c43ae536652696a8
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-02-13 23:24:04 +0630

    chore: remove wallet

--- src/views/Stake/WalletView.vue
+++ src/views/Stake/WalletView.vue
@@ -16,54 +16,6 @@
 
         <section class="md:flex mt-12 md:mb-5 gap-5 md:gap-10">
             <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectBscWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">Binance</span>
-                    <img src="/binance.svg" alt="Binance" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-            <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectCoinbaseWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">Coinbase</span>
-                    <img src="/coinbase.svg" alt="Coinbase" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-        </section>
-        <section class="md:flex md:mb-5 gap-5 md:gap-10">
-            <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectMetamaskWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">Kraken</span>
-                    <img src="/kraken.svg" alt="Kraken" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-            <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectMetamaskWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">Bybit</span>
-                    <img src="/bybit.svg" alt="Bybit" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-        </section>
-        <section class="md:flex md:mb-5 gap-5 md:gap-10">
-            <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectOkxWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">OKX</span>
-                    <img src="/okx.svg" alt="OKX" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-            <div class="hover:cursor-pointer w-full mb-3">
-                <div @click="connectMetamaskWallet"
-                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
-                    <span class="text-xl md:text-2xl">Crypto.com</span>
-                    <img src="/crypto.svg" alt="Crypto" class="w-[40px] h-[40px]">
-                </div>
-            </div>
-        </section>
-        <section class="md:flex md:mb-5 gap-5 md:gap-10">
-            <div class="hover:cursor-pointer w-full mb-3">
                 <div @click="connectMetamaskWallet"
                     class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
                     <span class="text-xl md:text-2xl">Metamask</span>
@@ -78,6 +30,57 @@
                 </div>
             </div>
         </section>
+
+        <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectOkxWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">OKX</span>
+                    <img src="/okx.svg" alt="OKX" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectCoinbaseWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Coinbase</span>
+                    <img src="/coinbase.svg" alt="Coinbase" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+        </section>
+
+        <!-- <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectMetamaskWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Kraken</span>
+                    <img src="/kraken.svg" alt="Kraken" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectMetamaskWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Bybit</span>
+                    <img src="/bybit.svg" alt="Bybit" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+        </section> -->
+
+        <!-- <section class="md:flex md:mb-5 gap-5 md:gap-10">
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectBscWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Binance</span>
+                    <img src="/binance.svg" alt="Binance" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+            <div class="hover:cursor-pointer w-full mb-3">
+                <div @click="connectMetamaskWallet"
+                    class="bg-tertiary rounded-md flex justify-between items-center px-6 py-4 md:px-10 md:py-5">
+                    <span class="text-xl md:text-2xl">Crypto.com</span>
+                    <img src="/crypto.svg" alt="Crypto" class="w-[40px] h-[40px]">
+                </div>
+            </div>
+        </section> -->
 
         <Toast />
     </div>
